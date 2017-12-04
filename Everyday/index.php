6d2f2f3a5b84e571c55a76c86982e29ed146f4c8
Владислав Львович Быконь <Hollow-oren@yandex.ru>
2017-12-04 18:40:59 +0500

    EveryDayFor... #delete

--- Everyday/index.php
+++ Everyday/index.php
@@ -5,25 +5,21 @@
 mb_internal_encoding('utf-8');
 error_reporting(E_ALL);
 
-function sortty()
-{
-    echo '1';
-}
 ?>
 
 <form name="top" method="post">
     <p><b><h2>Список дел на сегодня :</h2></b>
-        <input type="text" size="36" name="list"  placeholder="Описание задачи">
-        <input type="submit"  value="Добавить" >&nbsp;&nbsp;&nbsp;
+        <input type="text" size="36" name="list" placeholder="Описание задачи">
+        <input type="submit" value="Добавить">&nbsp;&nbsp;&nbsp;
 </form>
-    <form name="top3" method="post">
-         Сортировать по:
-        <select name="top2">
-            <option <?php if($_POST['top2'] == 'Дате добавления'): ?>selected<?php endif; ?>>Дате добавления</option>
-            <option <?php if($_POST['top2'] == 'Статусу'): ?>selected<?php endif; ?>>Статусу</option>
-            <option <?php if($_POST['top2'] == 'Описанию'): ?>selected<?php endif; ?>>Описанию</option>
-        </select>
-    <input type="submit"  value="Отсортировать">
+<form name="top2" method="post">
+    Сортировать по:
+    <select name="top2">
+        <option <?php if ($_POST['top2'] == 'Дате добавления'): ?>selected<?php endif; ?>>Дате добавления</option>
+        <option <?php if ($_POST['top2'] == 'Статусу'): ?>selected<?php endif; ?>>Статусу</option>
+        <option <?php if ($_POST['top2'] == 'Описанию'): ?>selected<?php endif; ?>>Описанию</option>
+    </select>
+    <input type="submit" value="Отсортировать">
     </p>
 </form>
 <?php
@@ -39,15 +35,14 @@ $pdo = new PDO("mysql:host=$host;dbname=$dataBase;charset=utf8", $user, $passwor
 $date = date("Y-m-d H:i:s");
 
 $sql2 = "INSERT INTO tasks VALUES (NULL , ? , 'В процессе', ?)";
-if (isset($_POST['list']))
-{
+if (isset($_POST['list'])) {
     $sql2 = "INSERT INTO tasks VALUES (NULL , ? , 'В процессе', ?)";
     $state = $pdo->prepare($sql2);
     $state->execute(array($_POST['list'], $date));
 }
-if (isset($_POST['top2']))
-{
-    switch ($_POST['top2']){
+
+if (isset($_POST['top2'])) {
+    switch ($_POST['top2']) {
         case 'Дате добавления' :
             $sql2 = "SELECT * FROM tasks ORDER BY date_added ASC";
             break;
@@ -60,7 +55,7 @@ if (isset($_POST['top2']))
     }
     $state = $pdo->prepare($sql2);
     $state->execute();
-}else{
+} else {
     $sql = "SELECT * FROM tasks";
     $state = $pdo->prepare($sql);
     $state->execute();
@@ -71,51 +66,51 @@ if (isset($_POST['top2']))
 <table align="left" width="600" border="1">
     <tr>
         <td>
-            <?php echo 'id';?>
+            <?php echo 'id'; ?>
         </td>
         <td>
-            <?php echo 'Описание задачи';?>
+            <?php echo 'Описание задачи'; ?>
         </td>
         <td>
-            <?php echo 'Статус';?>
+            <?php echo 'Статус'; ?>
         </td>
         <td>
-            <?php echo 'Дата добавления';?>
+            <?php echo 'Дата добавления'; ?>
         </td>
         <td>
-            <?php echo ' ';?>
+            <?php echo ' '; ?>
         </td>
     </tr>
     <?php
-foreach ($state as $row){
-?>
-    <tr>
-        <td>
-            <?php echo $row['id'];?>
-        </td>
-        <td>
-            <?php echo $row['description'];?>
-        </td>
-        <td>
-            <?php echo $row['is_done'];?>
-        </td>
-        <td>
-            <?php echo $row['date_added'];?>
-        </td>
-        <td>
+    foreach ($state as $row) {
+        ?>
+        <tr>
+            <td>
+                <?php echo $row['id']; ?>
+            </td>
+            <td>
+                <?php echo $row['description']; ?>
+            </td>
+            <td>
+                <?php echo $row['is_done']; ?>
+            </td>
+            <td>
+                <?php echo $row['date_added']; ?>
+            </td>
+            <td>
 
-            <a href=" __DIR__ . '/../index.php?id=<?php echo $row['id']?>&action=delete">Удалить</a>
+                <a href=" __DIR__ . '/../index.php?id=<?php echo $row['id'] ?>&action=delete">Удалить</a>
 
-        </td>
+            </td>
 
-    </tr>
+        </tr>
 
 
-    <?php
-}
-var_dump($_GET);
+        <?php
+    }
+    var_dump($_GET);
     var_dump($_POST);
-?>
+    ?>
 </table>
 </body>
 </html>
--- EverydayForCamps/index.php
+++ EverydayForCamps/index.php
@@ -5,33 +5,26 @@
 mb_internal_encoding('utf-8');
 error_reporting(E_ALL);
 
-function parsingHomeWork()
-{
-    $getHomeWork = file_get_contents('http://university.netology.ru/u/vfilipov/homework2.php');
-    file_put_contents('3.txt',$getHomeWork);
-}
-
-
 ?>
 
 <form name="top" method="post">
     <p><b><h2>Список дел на сегодня :</h2></b>
-        <input type="text" size="36" name="list"  placeholder="Описание задачи">
-        <input type="submit"  value="Добавить" >&nbsp;&nbsp;&nbsp;
+        <input type="text" size="36" name="list" placeholder="Описание задачи">
+        <input type="submit" value="Добавить">&nbsp;&nbsp;&nbsp;
 </form>
-    <form name="top3" method="post">
-         Сортировать по:
-        <select name="top2">
-            <option>Дате добавления</option>
-            <option>Статусу</option>
-            <option>Описанию</option>
-        </select>
-    <input type="submit"  value="Отсортировать">
+<form name="top2" method="post">
+    Сортировать по:
+    <select name="top2">
+        <option <?php if ($_POST['top2'] == 'Дате добавления'): ?>selected<?php endif; ?>>Дате добавления</option>
+        <option <?php if ($_POST['top2'] == 'Статусу'): ?>selected<?php endif; ?>>Статусу</option>
+        <option <?php if ($_POST['top2'] == 'Описанию'): ?>selected<?php endif; ?>>Описанию</option>
+    </select>
+    <input type="submit" value="Отсортировать">
     </p>
 </form>
 <?php
 $host = 'localhost';
-$dataBase = 'task2';
+$dataBase = 'tasks';
 $user = 'root';
 $password = '';
 
@@ -39,10 +32,90 @@ $pdo = new PDO("mysql:host=$host;dbname=$dataBase;charset=utf8", $user, $passwor
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
 
+$date = date("Y-m-d H:i:s");
 
+$sql2 = "INSERT INTO tasks VALUES (NULL , ? , 'В процессе', ?)";
+if (isset($_POST['list'])) {
+    $sql2 = "INSERT INTO tasks VALUES (NULL , ? , 'В процессе', ?)";
+    $state = $pdo->prepare($sql2);
+    $state->execute(array($_POST['list'], $date));
+}
+if (isset($_GET['delete'])) {
+    $sql2 = "DELETE FROM tasks WHERE id = ?";
+    $state = $pdo->prepare($sql2);
+    $state->execute(array($_GET['id']));
+}
+
+if (isset($_POST['top2'])) {
+    switch ($_POST['top2']) {
+        case 'Дате добавления' :
+            $sql2 = "SELECT * FROM tasks ORDER BY date_added ASC";
+            break;
+        case 'Статусу' :
+            $sql2 = "SELECT * FROM tasks ORDER BY is_done ASC";
+            break;
+        case 'Описанию' :
+            $sql2 = "SELECT * FROM tasks ORDER BY description ASC";
+            break;
+    }
+    $state = $pdo->prepare($sql2);
+    $state->execute();
+} else {
+    $sql = "SELECT * FROM tasks";
+    $state = $pdo->prepare($sql);
+    $state->execute();
+}
 
 
 ?>
+<table align="left" width="600" border="1">
+    <tr>
+        <td>
+            <?php echo 'id'; ?>
+        </td>
+        <td>
+            <?php echo 'Описание задачи'; ?>
+        </td>
+        <td>
+            <?php echo 'Статус'; ?>
+        </td>
+        <td>
+            <?php echo 'Дата добавления'; ?>
+        </td>
+        <td>
+            <?php echo ' '; ?>
+        </td>
+    </tr>
+    <?php
+    foreach ($state as $row) {
+        ?>
+        <tr>
+            <td>
+                <?php echo $row['id']; ?>
+            </td>
+            <td>
+                <?php echo $row['description']; ?>
+            </td>
+            <td>
+                <?php echo $row['is_done']; ?>
+            </td>
+            <td>
+                <?php echo $row['date_added']; ?>
+            </td>
+            <td>
+                <a href=" __DIR__ . '/../index.php?id=<?php echo $row['id'] ?>&edit">Изменить</a>
+                <a href=" __DIR__ . '/../index.php?id=<?php echo $row['id'] ?>&complete">Выполнить</a>
+                <a href=" __DIR__ . '/../index.php?id=<?php echo $row['id'] ?>&delete">Удалить</a>
 
+            </td>
+
+        </tr>
+
+
+        <?php
+    }
+    var_dump($_GET);
+    var_dump($_POST);
+    ?>
 </body>
 </html>
